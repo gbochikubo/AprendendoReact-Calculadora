66412ceae09fa2abbed460d905c45c5d617e83d6
gbochikubo <gbochikubo@estudante.ufla.br>
2020-06-11 09:47:32 -0300

    projeto calculadora finalizado

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<Calculator></Calculator>,
   document.getElementById('root')
 );
 
